910258242b2fd598f641dbb661fa7f90fce903c4
etfnordic <etfnordic@gmail.com>
2025-12-26 22:46:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const ws = new WebSocket("wss://train.etfnordic.workers.dev/ws?v=1");
 
-ws.onopen = () => console.log("Connected to proxy");
+ws.onopen = () => console.log("Connected ✅");
 ws.onmessage = (ev) => console.log("msg:", ev.data);
 ws.onerror = (e) => console.error("ws error", e);
-ws.onclose = (e) => console.log("ws closed", e.code, e.reason);
+ws.onclose = (e) => console.log("closed", e.code, e.reason);
