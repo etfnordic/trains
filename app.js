bffb6ce7afd6e433e441936ce23f38151f70148d
etfnordic <etfnordic@gmail.com>
2025-12-26 23:29:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 const ws = new WebSocket("wss://train.etfnordic.workers.dev/echo");
 ws.onopen = () => { console.log("echo open"); ws.send("hello"); };
 ws.onmessage = (e) => console.log("echo msg:", e.data);
-ws.onclose = (e) => console.log("echo close:", e.code, e.reason);
-ws.onerror = (e) => console.log("echo error", e);
